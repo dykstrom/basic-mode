dykstrom/basic-mode

commit f68d4e223d27beef32fd8d9ab2e2fc85d2900075

Fix loop condition, and consider whitespace-only lines to be empty.

src/basic-mode.el
@@ -462,9 +462,8 @@ buffer if only the active region is renumbered."
 	current-line-number)
     (save-excursion
       (goto-char point-start)
-      (while (and (not (eobp))
-		  (<= (point) point-end))
-	(unless (looking-at "^$")
+      (while (not (eobp))
+	(unless (looking-at "^[ \t]*$")
 	  (setq current-line-number (string-to-number (basic-remove-line-number)))
 	  (let ((jump-locations (gethash current-line-number jump-list)))
 	    (save-excursion
@@ -477,8 +476,7 @@ buffer if only the active region is renumbered."
 	  (beginning-of-line)
 	  (insert (basic-format-line-number new-line-number))
 	  (setq new-line-number (+ new-line-number increment)))
-	(forward-line 1)
-	(end-of-line)))
+	(forward-line 1)))
     (maphash (lambda (target sources)
 	       (dolist (m sources)
 		 (when (marker-position m)

commit ddf24fdd9122eb413f6e110ce5b2fc94aa37e359

Fix renumbering of regions
f68d4e2 broke the renumber region functionality - renumbering
continued past the end of the region.  This patch restores the
functionality.

src/basic-mode.el
@@ -472,7 +472,8 @@ have numbers are included in the renumbering.
 	(point-end (if (use-region-p) (region-end) (point-max))))
     (save-excursion
       (goto-char point-start)
-      (while (not (eobp))
+      (while (and (not (eobp))
+		  (< (point) point-end))
 	(unless (looking-at "^[ \t]*$")
 	  (let ((current-line-number (string-to-number (basic-remove-line-number))))
 	    (when (or basic-renumber-unnumbered-lines